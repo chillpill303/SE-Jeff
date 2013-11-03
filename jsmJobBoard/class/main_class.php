ec841a04fc6f51454a0f97ca97ec14f841e3fa82
jeffreysmattson <jsmattson@plymouth.edu>
2013-11-02 20:41:43 -0400

    Update main_class.php
    Cleaned up this file. This is WORKING CODE! for Thursday.

--- jsmJobBoard/class/main_class.php
+++ jsmJobBoard/class/main_class.php
@@ -33,13 +33,10 @@ class Set {
 		//the action that creates the custom post type
 		add_action( 'init', array($this, 'create_post_type')); 
 	
-		//register taxonomy for the custom post
-//		register_taxonomy("Jobs", array("job_posting"), array("hierarchical" => true, "label" =>"Job", "singular_label" => "Job", "rewrite" => false));
-		
 		//this is to add custom fields in the custom post.
 		add_action("admin_init", array($this, "admin_init"));
 
-		//redirect to theme file
+		//redirect to theme file this does not work!!!!!!! dont know why
 		add_action("template_redirect", array($this, 'my_theme_redirect'));
 
 		//for changing the page template after it is created.
@@ -51,14 +48,14 @@ class Set {
 	}
 	
 	
-	//this function creats a custom post option in the admin menu called Job Posting
+	//this function creates a custom post option in the admin menu called Job Posting
 	public function create_post_type() {
 		
 		//register_post_type puts it all together. Google all the options
 		register_post_type( 'job_posting',
 			array(
 				'labels' => array(
-					'name' => __( 'Job Posting' ),
+					'name' => __( 'Job Postings' ),
 					'singular_name' => __( 'Job Posting' ),
 					'add_new_item' => __( 'Add New Job Posting')
 				),
@@ -67,7 +64,6 @@ class Set {
 			'show_in_nav_menus' => true,
 			'show_ui' => true,
 			'map_meta_cap' => true,
-		//	'taxonomies' => array('category', 'page'),
 			'capability_type' => 'post',
 			'has_archive' => true, 
 			'hierarchical' => false,
@@ -77,7 +73,7 @@ class Set {
 		);
 	}
 	
-	//function that is called by the add_action to create the custom field in the custom post.
+	//function that is called by the add_action to create the custom fields in the custom post.
 	public function admin_init(){
 		add_meta_box("job_requirements-meta",__("Job Requirements","job_requirements"),array( $this, 'job_requirements'), "job_posting", "normal" , "high");
 		//for start date and end date
@@ -90,60 +86,36 @@ class Set {
 		$custom = get_post_custom($post->ID);
 		$job_requirements = get_post_meta( $post->ID, 'job_requirements', true);
 		?>
-		<!-- <p><label>Job Requirements</label><br /> -->
 		<textarea cols="50" rows="5" wrap="virtual" name="job_requirements"> <?php echo esc_attr( $job_requirements ); ?> </textarea></p>
 		<?php
-		//nonce field to check for later
+		
+		//nonce field to check for later this didn't work but we may need it later
 		wp_nonce_field( 'job_requirements' , 'job_requirements_nonce');
 
 	} 	
 
-	//for the start_date meta box.
+	//for the start/end_date meta box.
 	public function start_date(){
 		global $post;
 		$custom = get_post_custom($post->ID);
 		$start_date = get_post_meta($post->ID, 'start_date', true);
 		$end_date = get_post_meta($post->ID, 'start_date', true);
-		?>
+?>
+
 		<p><label>Start Date</label><br />
 		<input type="date" name="start_date" value="<?php echo $start_date; ?>"</p>
+		
 		<p><label>End Date</label><br />
 		<input type="date" name="end_date" value="<?php echo $end_date; ?>"</p> 
-		<?php
+		
+<?php
 	}
 
 
 	//save the new meta box data so it shows on the post
 	public function save_meta_post_data($post_id) {
-		/*if (! isset( $_POST['job_requirements_nonce'])) {
-			return $post_id;
-		}
-
-		$nonce = $_POST['job_requirements_nonce'];
-
-		//verify nonce
-		if (!wp_verify_nonce($nonce, 'job_requirements')) {
-			return $post_id;
-		}
-
-		//if autosave form has not been submitted
-		if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-			return $post_id;
-		}
-
-		//check users permissions
-		if ('page' == $_POST['job_posting']) {
-			if ( !current_user_can('edit_page', $post_id)) {
-				return $post_id;
-
-			} else {
-				if (! current_user_can( 'edit_post', $post_id)) {
-					return $post_id;
-				}
 		
-		// Sanitize user input.
-		$mydata = sanitize_text_field( $_POST['job_requirements']);*/
-
+		//PROBABLY SOME SECURITY ISSUES WITH THIS BUT IT WORKS FOR NOW
 		//post the job requirements if there are some other wise post none. It looks better.
 		global $post;
 		if (get_post_meta( $post->ID, 'job_requirements', true) == '') {
@@ -155,10 +127,8 @@ class Set {
 			update_post_meta($post->ID, 'job_requirements', $_POST["job_requirements"]);
 			update_post_meta($post->ID, 'start_date' , $_POST["start_date"]);
 			update_post_meta($post->ID, 'end_date' , $_POST["end_date"]);
-
 		}
 
-	//	update_post_meta( $post_id, 'job_requirements', $mydata);
 	}
 	
 	// creates new page when the plugin is activated
@@ -171,7 +141,6 @@ class Set {
 
 			$my_page = array(
 				'post_title' => $page_title,
-			//	'post_content' => 'This is were the new job posting will end up',
 				'post_status' => 'publish',
 				'post_type' => 'page',
 				'post_author' => 2,
@@ -201,30 +170,10 @@ class Set {
 		wp_delete_post($page->ID, true);
 	}
 	
-	//for template redirect
+	//for template redirect !!!!!DOES NOT WORK !!!!!
 	function my_theme_redirect() {
 	    global $wp;
 	    $plugindir = plugin_dir_path( __FILE__ );
-
-	    /*A Specific Custom Post Type
-	    if ($wp->query_vars["post_type"] == 'job_posting') {
-		$templatefilename = 'job_post_template.php';
-		if (file_exists(TEMPLATEPATH . '/' . $templatefilename)) {
-		    $return_template = TEMPLATEPATH . '/' . $templatefilename;
-		} else {
-		    $return_template = $plugindir . '/pagetemp/' . $templatefilename;
-		}
-		do_theme_redirect($return_template);
-
-	    //A Custom Taxonomy Page
-	    } elseif ($wp->query_vars["taxonomy"] == 'product_categories') {
-		$templatefilename = 'taxonomy-product_categories.php';
-		if (file_exists(TEMPLATEPATH . '/' . $templatefilename)) {
-		    $return_template = TEMPLATEPATH . '/' . $templatefilename;
-		} else {
-		    $return_template = $plugindir . '/themefiles/' . $templatefilename;
-		}
-		do_theme_redirect($return_template);*/
 
 	    //A Simple Page
 	    if ($wp->query_vars["pagename"] == 'Job Postings') {
